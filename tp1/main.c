b32f02ae71aa2308e13ad4bb77510d49407ac2a3
SDB (Laleft) <sdebernardez@gmail.com>
2023-09-17 14:01:36 -0300

    Remove locale settings

--- tp1/main.c
+++ tp1/main.c
@@ -7,20 +7,12 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <locale.h>
-#include <windows.h>
 
 #include "config/colors.c"
 #include "config/settings.c"
 #include "lib/print_breakline.c"
 
 int main(void) {
-    setlocale(LC_ALL, "spanish");
-    SetConsoleCP(1252);
-    SetConsoleOutputCP(1252);
-
     system("cls");
 
     char cadena[MAX_INPUT_LENGTH];
