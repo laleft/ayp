17868d18e3f628e16dc5a5e99ff8039b89761fe9
Santiago De Bernardez <sdebernardez@gmail.com>
2023-09-18 16:47:36 -0300

    Random number functionality

--- tp1/main.c
+++ tp1/main.c
@@ -7,6 +7,9 @@
 */
 
 #include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "config/colors.c"
 #include "config/settings.c"
@@ -14,12 +17,34 @@
 
 int main(void) {
     system("cls");
+    setlocale(LC_ALL, "");  
 
-    char cadena[MAX_INPUT_LENGTH];
-    printf("%s%s%s%s", "Bienvenido a ", COLOR_CYAN, "MUNERO", COLOR_RESET ", un juego en el que deberás adivinar un número de 4 cifras que elegiré al azar\n");
+    printf("%s%s%s%s\n", "Bienvenido a ", COLOR_CYAN, "MUNERO", COLOR_RESET ", un juego en el que deberás adivinar un número de 4 cifras que elegiré al azar");
     printf("%s", COLOR_YELLOW);
     printBreakline(2);
-    system("pause");
+    // system("pause");
     printf("%s", COLOR_RESET);
+
+    char munero[4]; // número a adivinar
+    int random;    // numero random
+    int completado = 0; // contador
+    time_t reloj; // semilla para rand()
+    srand((unsigned) (&reloj));
+
+    random = rand() % 10;
+    for(int x = 0; x < 100; x++) {
+        printf("%d ", rand() % 10);
+    }
+    
+    while(completado < 4) {
+        printf("%d", random);
+        munero[completado] = random;
+        completado++;
+    }
+    
+    printf("El numero es ");
+    for(int i = 0; i < 4; i++) {
+        printf("%d", munero[i]);
+    }
     return 0;
 }
