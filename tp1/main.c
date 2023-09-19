2d41c9933b20443ec15f287711634c7ad641c26f
Santiago De Bernardez <sdebernardez@gmail.com>
2023-09-19 16:30:13 -0300

    Update checkNotEqualNumbers function

--- tp1/main.c
+++ tp1/main.c
@@ -18,41 +18,50 @@
 
 
 int checkNotEqualNumbers(char numbers[], int start_at) {
+    if(strlen(numbers) == start_at) {
+        return 0;
+    }
+    int flag = 0;
     for(int i = start_at; i < strlen(numbers) - 1; i++) {
+        printf("Arranca en %d\n", start_at);
         printf("%c - %c ", numbers[start_at], numbers[i + 1]);
         if(numbers[start_at] == numbers[i + 1]) {
             printf("Son iguales\n");
-            return 0;
-        } else {
-            checkNotEqualNumbers(numbers, start_at + 1);
-            printf("Son distintos\n");
-            return 1;
-        }
+            flag = 1;
+            break;
+        } 
     }
+    if(flag == 1) {
+        return flag;
+    } else {
+        flag = checkNotEqualNumbers(numbers, start_at + 1);
+    }
+    return flag;
 }
 
 int main(void) {
     setlocale(LC_ALL, "");  
 
-    printf("%s%s%s%s\n", "Bienvenido a ", COLOR_CYAN, "MUNERO", COLOR_RESET ", un juego en el que deberás adivinar un número de 4 cifras que elegiré al azar");
+    printf("%s%s%s%s\n", "Bienvenido a ", COLOR_CYAN, "MUNERO", COLOR_RESET ", un juego en el que deberï¿½s adivinar un nï¿½mero de 4 cifras que elegirï¿½ al azar");
     printf("%s", COLOR_YELLOW);
     printBreakline(2);
     // system("pause");
     printf("%s", COLOR_RESET);
 
-    char munero[4]; // número a adivinar
-    int f_random = 0;    // numero random
+    char munero[4]; // nï¿½mero a adivinar
+    int f_random = 1;    // flag numero random
 
     srand(time(NULL)); // usa el reloj como semilla
     
-    while(f_random == 0) {
+    while(f_random == 1) {
         int random_1 = rand() % 10;
         int random_2 = rand() % 10;
         int random_3 = rand() % 10;
         int random_4 = rand() % 10;
         sprintf(munero, "%d%d%d%d", random_1, random_2, random_3, random_4);
         f_random = checkNotEqualNumbers(munero, 0);
-        printf("%d", f_random);
+        printf("Valor del flag: %d\n", f_random);
+        // f_random = 0;
     }
     
     printf("El numero es %s", munero);
