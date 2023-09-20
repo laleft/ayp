d3afac9303af884e35a313103baeee9ba1a58224
SDB (Laleft) <sdebernardez@gmail.com>
2023-09-19 23:16:56 -0300

    Create findRandomNumber function

---
+++ tp1/lib/find_random_number.c
@@ -0,0 +1,18 @@
+#include "check_not_equal_numbers.c"
+
+extern int debug;
+
+char findRandomNumber() {
+    int f_random = 1; // Flag
+    char numero[4];
+    while(f_random == 1) {
+        int random_1 = rand() % 10;
+        int random_2 = rand() % 10;
+        int random_3 = rand() % 10;
+        int random_4 = rand() % 10;
+        sprintf(numero, "%d%d%d%d", random_1, random_2, random_3, random_4);
+        if (debug) printf("El numero es %s\n", numero);
+        f_random = checkNotEqualNumbers(numero, 0);
+    }
+    return numero;
+}
