545804ef0fe7218dc44a07ed2471adff6704a544
SDB (Laleft) <sdebernardez@gmail.com>
2023-09-28 08:32:39 -0300

    Add helper functiopns

--- tp1/main.c
+++ tp1/main.c
@@ -16,6 +16,7 @@
 #include "config/settings.c"
 #include "lib/check_not_equal_numbers.c"
 #include "lib/print_breakline.c"
+#include "lib/helpers.c"
 
 int debug = 0; // variable global para activar modo debug
 
@@ -31,15 +32,16 @@ int main(int argc, char *argv[])
     }
 
     /* InicializaciOn */
-    setlocale(LC_ALL, ""); // No s� si funciona
+    setlocale(LC_ALL, ""); // No sE si funciona
     char munero[4];        // nUmero a adivinar
     int f_random = 0;      // flag numero random (toma valor true cuando el nUmero sea random)
     srand(time(NULL));     // usa el reloj como semilla
+    char name[50];
 
     /* PresentaciOn */
     if (debug == 0)
     {
-        system("cls"); // solo para Windows
+        // system("cls"); // solo para Windows
     }
     printf("%s", COLOR_YELLOW);
     printf("\n%s%s%s%s", "BIENVENIDO A ", COLOR_CYAN, "MUNERO", COLOR_YELLOW ", UN JUEGO EN EL QUE DEBERAS ADIVINAR UN NUMERO DE 4 CIFRAS QUE ELEGIRE AL AZAR");
@@ -55,10 +57,13 @@ int main(int argc, char *argv[])
             putchar(t);
         fclose(file);
     }
-    system("pause");
-    printBreakline(2);
-    printf("GENERANDO NUMERO ALEATORIO: ****\n");
-    printBreakline(2);
+    printf("INGRESA TU NOMBRE: ");
+    fgets(name, sizeof(name), stdin);
+    printBreakline(1);
+    printf(">>> HOLA, %s", name);
+    printBreakline(1);
+    printf("GENERANDO NUMERO ALEATORIO...\n");
+    printBreakline(1);
 
     /* Busca un nUmero de 4 cifras aleatoreas no repetidas */
     while (f_random == 0)
@@ -72,17 +77,23 @@ int main(int argc, char *argv[])
             printf("\nNumero %s\n", munero);
         f_random = checkNotEqualNumbers(munero, 0);
     }
+    printf("NUMERO GENERADO: ****\n");
+    printBreakline(1);
 
+    /* Variables del loop */
     char input[5];
     int i;
     int attempts = 0;
     int success = 0;
+    /* Mientras no supere los intentos y no adivine el numero */
     while (attempts < MAX_ATTEMPTS && success != 1)
     {
         printf("INGRESA 4 DIGITOS DIFERENTES: ");
         if (fgets(input, sizeof(input), stdin))
         {
-            fflush(stdin);
+            fflush(stdin); // Limpia el buffer del standard input
+
+            /* Si el usuario ingresa "fin" sale del programa*/
             if (strcmp(strupr(input), "FIN\n") == 0)
             {
                 printBreakline(2);
@@ -90,9 +101,9 @@ int main(int argc, char *argv[])
                 printBreakline(2);
                 exit(0);
             }
+
             if (1 == sscanf(input, "%d", &i))
             {
-                printf("CANTIDAD DE DIGITOS: %d\n", sizeof(input) / sizeof(input[0]));
                 if (sizeof(input) < 4)
                 {
                     printf(">>> DEBE INGRESAR 4 DIGITOS!\n");
@@ -100,7 +111,7 @@ int main(int argc, char *argv[])
                 }
                 if (checkNotEqualNumbers(input, 0) == 0)
                 {
-                    printf(">>> LOS DIGITOS NO PUEDEN REPETIRSE!\n", input);
+                    printError(">>> LOS DIGITOS NO PUEDEN REPETIRSE!\n");
                 }
                 else
                 {
@@ -111,7 +122,7 @@ int main(int argc, char *argv[])
             }
             else
             {
-                printf(">>> SOLO DEBE INGRESAR DIGITOS!\n");
+                printError(">>> SOLO DEBE INGRESAR DIGITOS!\n");
             }
         }
     }
