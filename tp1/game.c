a7cad3baec35644f5e038525ea59afee3ed0c5b8
Santiago De Bernardez <sdebernardez@gmail.com>
2023-09-29 00:52:56 -0300

    Add comments

--- tp1/game.c
+++ tp1/game.c
@@ -7,9 +7,9 @@ extern int debug;
 
 int game()
 {
-    char munero[4];   // nUmero a adivinar
-    int f_random = 0; // flag (toma valor true cuando el nUmero sea random)
-    srand(time(NULL));
+    char munero[4];    // nUmero a adivinar
+    int f_random = 0;  // flag (toma valor true cuando el nUmero sea random)
+    srand(time(NULL)); // usa el reloj como semilla
 
     printf("GENERANDO NUMERO ALEATORIO...\n");
     BR(1);
@@ -35,16 +35,17 @@ int game()
         printf("****");
     RST;
     BR(2);
-    /* Variables del loop */
-    char input[5];
+
+    char input[5]; // input de usuario
     int i;
-    int attempts = 0;
+    int attempts = 0; // contador de intentos
     time_t start_time, end_time;
-    /* Mientras no supere los intentos y no adivine el numero */
-    time(&start_time);
+
+    time(&start_time); // toma el tiempo de inicio de la ronda
     while (attempts < MAX_ATTEMPTS)
     {
         fflush(stdin); // vacIa el buffer
+
         printf("INGRESA 4 DIGITOS: ");
         if (fgets(input, sizeof(input), stdin))
         {
@@ -54,8 +55,9 @@ int game()
                 BR(2);
                 printf("*** HASTA LA PROXIMA! ***");
                 BR(2);
-                exit(0);
+                exit(0); // sale del programa
             }
+
             /* Chequea que los primeros 4 caracteres sean dIgitos */
             if (1 == sscanf(input, "%d", &i) && checkAllAreDigits(input, 4))
             {
@@ -71,26 +73,26 @@ int game()
                     int wrong = 4;
                     int right = 0;
                     int regular = 0;
-                    for (int x = 0; x < sizeof(input - 1); x++)
+                    for (int x = 0; x < sizeof(input - 1); x++) // recorre el input
                     {
-                        for (int y = 0; y < sizeof(munero - 1); y++)
+                        for (int y = 0; y < sizeof(munero - 1); y++) // recorre el numero aleatorio
                         {
-                            if (input[x] == munero[y])
+                            if (input[x] == munero[y]) // coicide el nUmero
                             {
                                 if (debug)
                                     printf("[DEBUG] POSICION COINCIDENCIAS [%d, %d](%c = %c)\n", x, y, input[x], munero[y]);
-                                if (x == y)
+                                if (x == y) // coincide la posiciOn
                                 {
-                                    right++;
+                                    right++; // +1 bien
                                 }
                                 else
                                 {
-                                    regular++;
+                                    regular++; // +1 regular
                                 }
                             }
                         }
                     }
-                    wrong -= (right + regular);
+                    wrong -= (right + regular); // calcula los dIgitos incorrectos
                     printf(">>> ");
                     if (wrong == 4)
                     {
@@ -98,6 +100,7 @@ int game()
                     }
                     else if (right == 4)
                     {
+                        /* Calcula el intervalo de tiempo */
                         time(&end_time);
                         double total_time_in_seconds = difftime(end_time, start_time);
                         int minutes = total_time_in_seconds / 60;
@@ -120,7 +123,7 @@ int game()
                         printf("[DEBUG] INTENTO: %d\n", attempts);
                 }
             }
-            else
+            else // Si no son dIgitos
             {
                 ERR(">>> DEBES INGRESAR 4 DIGITOS!\n");
             }
