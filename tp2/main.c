f4301c43e9f9f96e90e66232ff9bc8db2958fede
Santiago De Bernardez <sdebernardez@gmail.com>
2023-11-26 21:04:51 -0300

    Add game select

--- tp2/main.c
+++ tp2/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <windows.h>
 #include "config/const.c"
-#include "utils/prompt_for_name.c"
-#include "modules/launch.c"
+#include "modules/prompt_for_name.c"
+#include "modules/welcome.c"
 #include "games/munero.c"
+#include "games/ahorcado.c"
 #include "modules/select_game.c"
 
 int debug = 0; // variable global para activar modo debug
@@ -18,19 +19,49 @@ int main(int argc, char *argv[])
         }
     }
 
-    char *user_name = launch(); // Arranque del programa.
+    int selected_game;
+    char *user_name = NULL;
 
-    int selected_game = selectGame();
-
-    switch (selected_game)
+    // Ejecutar el programa hasta que se seleccione la opciOn 3
+    do
     {
-    case 1:
-        munero();
-        break;
+        RST; // Resetea el color de la consola
+        if (!debug)
+            CLS; // Limpia la pantalla
 
-    default:
-        break;
-    }
+        // Dibuja la presentaciOn
+        YLW;
+        printBox("<<<<<< AYP GAMES 0.1 >>>>>>\n(sdebernardez)", -1, '#');
+        RST;
+        BR(2);
 
-    return 0;
+        // Chequea si ya se ingresO un nombre de usuario
+        if (!user_name)
+        {
+            // Si no hay nombre de usuario se solicita
+            // y se dibuja una bienvenida
+            user_name = welcome();
+        }
+        else
+        {
+            printf("ESTA JUGANDO: %s\n\n", user_name);
+        }
+
+        // Seleccionar opciOn
+        selected_game = selectGame();
+
+        switch (selected_game)
+        {
+        case 1:
+            munero();
+            break;
+
+        case 2:
+            ahorcado();
+            break;
+        case 3:
+            return 0;
+            break;
+        };
+    } while (selected_game != 3);
 }
