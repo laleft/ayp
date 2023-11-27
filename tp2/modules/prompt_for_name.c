c24b0e4b188520724c9aeed973be6e8ecc8338d7
Santiago De Bernardez <sdebernardez@gmail.com>
2023-11-26 21:03:50 -0300

    Create promp_for_name function

---
+++ tp2/modules/prompt_for_name.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+/*
+ * Solicita nombre de usuario
+ *
+ * DescripciOn:
+ * FunciOn para solicitar el ingreso por teclado del nombre de usuario
+ *
+ * ParAmetros:
+ * - input: puntero de tipo char donde se almacenarA el nombre de usuario
+ * - max_username_length: logitud mAxima de la cadena a almacenar
+ *
+ * Retorna:
+ *  - sin valor de retorno (void)
+ *
+ */
+
+#define ANON "AnOnimo"
+
+void promptForName(char *input, int max_username_lenth)
+{
+    /* Solicita nombre de usuario (o anOnimo) */
+    printf("INGRESA TU NOMBRE: ");
+    fgets(input, max_username_lenth, stdin);
+    if (strcmp(input, "\n") == 0)
+        strcpy(input, ANON);
+
+    input[strcspn(input, "\n")] = 0; // Elimina el salto de lInea al final
+}
